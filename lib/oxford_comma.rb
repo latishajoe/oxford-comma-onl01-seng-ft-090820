863986ae7855bedc5998d671b21c6097c3bf9399
Latisha Joe <latisha_joe@yahoo.com>
2020-09-16 04:20:21 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,8 @@
 def oxford_comma(array)
   if element.length == 2
-    << element.join("and")
+    element.join("and")
   elsif element.length >= 3
-    << element.join("," "and")
+    element.join("," "and")
   else
      returns array
  end
